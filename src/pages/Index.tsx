484394917a67cadc76083c2c83a8d7c811725be1
ranjam <ryanzam2005@gmail.com>
2025-06-18 10:03:55 +0545

    fix build errors

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,4 +1,4 @@
-import { use, useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { Button } from '../components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '../components/ui/card';
 import { QrCode, ShoppingCart, Star, TrendingUp, Users } from 'lucide-react';
